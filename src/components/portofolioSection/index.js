9eed66760917a3e56157214a0d967e1431a2d05f
Fayssal Developer <fmekki2002@gmail.com>
2023-04-05 18:10:40 +0100

    portfolio v1.2 fixing portfolio section

--- src/components/portofolioSection/index.js
+++ src/components/portofolioSection/index.js
@@ -7,7 +7,7 @@ const PortfolioSection = () => {
     <section
       dir="rtl"
       id="portfolio"
-      class="bg-white px-10 my-8 flex justify-center items-center flex-col dark:bg-gray-900 text-center"
+      class="bg-white px-10 my-8 flex justify-center text-right items-center flex-col dark:bg-gray-900 text-center"
     >
       <h2 class="text-3xl font-bold text-center text-gray-800 dark:text-white">
         معرض اعمالي
@@ -15,7 +15,7 @@ const PortfolioSection = () => {
       <h4 class="mb-12 text-xl font-normal text-center text-gray-500 dark:text-gray-300 ">
         بعض الاعمال التي قمت بها
       </h4>
-      <div class=" hidden  flex-row items-center max-w-5xl mb-6 text-center bg-black  rounded-lg   dark:bg-gray-800 dark:border-gray-700 md:flex transition hover:scale-105">
+      <div style={{transition:"0.3s ease-in-out"}} class=" hidden  flex-row items-center max-w-5xl mb-6 text-center bg-black  rounded-lg   dark:bg-gray-800 dark:border-gray-700 md:flex transition hover:scale-105">
         <img
           class="object-cover h-72 w-96 md:h-56 md:w-80 rounded-r-md "
           src={ProjectImg01}
@@ -51,7 +51,7 @@ const PortfolioSection = () => {
         </div>
       </div>
 
-      <div class=" hidden  flex-row items-center max-w-5xl mb-6 text-center bg-white  rounded-lg border dark:bg-gray-800 dark:border-gray-700 md:flex transition hover:scale-105">
+      <div style={{transition:"0.3s ease-in-out"}} class=" hidden  flex-row items-center max-w-5xl mb-6 text-center bg-white  rounded-lg border dark:bg-gray-800 dark:border-gray-700 md:flex transition hover:scale-105">
         <img
           class="object-cover h-72 w-96 md:h-56 md:w-80 rounded-r-md "
           src={ProjectImg02}
@@ -59,10 +59,10 @@ const PortfolioSection = () => {
         />
         <div class="flex flex-col items-start justify-between p-8 text-right leading-normal">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-black dark:text-white">
-          قالب مواقع لورشات السيارات
+            قالب مواقع لورشات السيارات
           </h5>
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          قالب مواقع لورشات اصلاح السيارات static باستعمال html css js
+            قالب مواقع لورشات اصلاح السيارات static باستعمال html css js
           </p>
           <a
             href="https://mekki14.github.io/car-repair/"
@@ -88,7 +88,7 @@ const PortfolioSection = () => {
       </div>
 
 
-      <div class=" hidden  flex-row items-center max-w-6xl mb-6 text-center  rounded-lg   dark:bg-gray-800 dark:border-gray-700 md:flex transition hover:scale-105" style={{backgroundColor:'#FCF2EC'}}>
+      <div class=" hidden  flex-row items-center max-w-6xl mb-6 text-center  rounded-lg   dark:bg-gray-800 dark:border-gray-700 md:flex transition hover:scale-105" style={{ backgroundColor: '#FCF2EC', transition : "0.3s ease-in-out" }}>
         <img
           class="object-cover h-96 w-96 md:h-56 md:w-96 rounded-r-md "
           src={ProjectImg03}
@@ -96,10 +96,10 @@ const PortfolioSection = () => {
         />
         <div class="flex flex-col items-start justify-between p-8 text-right leading-normal">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-black dark:text-white">
-          موقع demo تجارة الكترونية
+            موقع demo تجارة الكترونية
           </h5>
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          موقع غير حقيقي لبيع الساعات باستعمال html css js
+            موقع غير حقيقي لبيع الساعات باستعمال html css js
           </p>
           <a
             href="https://mekki14.github.io/luxwa"
@@ -122,7 +122,119 @@ const PortfolioSection = () => {
             </svg>
           </a>
         </div>
+
+
+
       </div>
+ {/* start */}
+
+ <a href="#https://mekki14.github.io/luxwa" class="group relative block mb-6 bg-black rounded-lg md:hidden">
+  <img
+    alt="Developer"
+    src={ProjectImg03}
+    class="absolute inset-0 h-full w-full object-cover opacity-90 transition-opacity group-hover:opacity-50"
+  />
+
+  <div class="relative p-4 sm:p-6 lg:p-8">
+    
+    <div class="mt-32 sm:mt-48 lg:mt-64">
+      <div
+        class="flex flex-col justify-center items-start w-96 -translate-x-8 transform opacity-0 transition-all group-hover:translate-x-0 group-hover:opacity-100"
+      >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">
+            موقع demo تجارة الكترونية
+          </h5>
+          <p class="mb-3 font-normal text-gray-100 dark:text-gray-400">
+            موقع غير حقيقي لبيع الساعات باستعمال html css js
+          </p>
+          <a
+            href="https://mekki14.github.io/luxwa"
+            dir="rtl"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            عرض المزيد
+            </a>
+      </div>
+    </div>
+  </div>
+</a>
+
+ {/* end */}
+
+
+  {/* start */}
+
+  <a href="https://lapiontb.onrender.com/" class="group relative block mb-6 bg-black rounded-lg md:hidden">
+  <img
+    alt="Developer"
+    src={ProjectImg01}
+    class="absolute inset-0 h-full w-full object-cover opacity-90 transition-opacity rounded-lg group-hover:opacity-50"
+  />
+
+  <div class="relative p-4 sm:p-6 lg:p-8">
+    
+    <div class="mt-32 sm:mt-48 lg:mt-64">
+      <div
+        class="flex flex-col justify-center items-start w-96 -translate-x-8 transform opacity-0 transition-all group-hover:translate-x-0 group-hover:opacity-100"
+      >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">
+        تصميم موقع لمطعم
+          </h5>
+          <p class="mb-3 font-normal text-gray-100 dark:text-gray-400">
+          تصميم و تطوير موقع الكتروني لاحدى المطاعم باستعمال Mern stack.
+          </p>
+          <a
+            href="https://lapiontb.onrender.com/"
+            dir="rtl"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            عرض المزيد
+            </a>
+      </div>
+    </div>
+  </div>
+</a>
+
+ {/* end */}
+
+
+
+  {/* start */}
+
+  <a href="https://mekki14.github.io/car-repair" class="group relative block bg-black rounded-lg md:hidden">
+  <img
+    alt="Developer"
+    src={ProjectImg02}
+    class="absolute inset-0 h-full w-full object-cover opacity-90 transition-opacity rounded-lg group-hover:opacity-50"
+  />
+
+  <div class="relative p-4 sm:p-6 lg:p-8">
+    
+    <div class="mt-32 sm:mt-48 lg:mt-64">
+      <div
+        class="flex flex-col justify-center items-start w-96 -translate-x-8 transform opacity-0 transition-all group-hover:translate-x-0 group-hover:opacity-100"
+      >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">
+        قالب مواقع لورشات السيارات
+          </h5>
+          <p class="mb-3 font-normal text-gray-100 dark:text-gray-400">
+          قالب مواقع لورشات اصلاح السيارات static باستعمال html css js
+          </p>
+          <a
+            href="https://mekki14.github.io/car-repair"
+            dir="rtl"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            عرض المزيد
+            </a>
+      </div>
+          
+    </div>
+  </div>
+</a>
+
+ {/* end */}
+
 
     </section>
   );
