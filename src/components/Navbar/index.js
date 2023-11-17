297768fccf1f8acaadef4e01a43a19dcbcc0869d
Fayssal Developer <58825362+mekki14@users.noreply.github.com>
2023-11-17 11:21:31 +0000

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -58,7 +58,7 @@ const Navbar = () => {
                   </ul>
               </div>
               <div className="hidden md:inline-block">
-                <a href="#contact" className="py-2 px-4 text-white bg-blue-600 hover:bg-blue-700 rounded-sm shadow">
+                <a href="/#contact" className="py-2 px-4 text-white bg-blue-600 hover:bg-blue-700 rounded-sm shadow">
                     تواصل معي
                 </a>
               </div>
